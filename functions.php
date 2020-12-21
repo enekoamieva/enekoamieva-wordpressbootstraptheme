f270ea6383749a74abb4d833738cfe5ebc5f1ba9
enekoamieva <enekoamieva@gmail.com>
2020-12-21 15:42:49 +0100

    03- Add Bootstrap 4.5.3

--- functions.php
+++ functions.php
@@ -7,10 +7,21 @@
  */
 
 function themecurso_enqueue_scripts() {
-    wp_register_style( 'style-css', get_stylesheet_uri(), [], '1.0.0'  );
-    wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], '1.0.0', true );
 
+    //Register Styles
+    wp_register_style( 'style-css', get_stylesheet_uri(), [], '1.0.0'  );
+    wp_register_style( 'bootstrap-css', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css', [], false, 'all'  );
+    
+    //Register Scripts
+    wp_register_script( 'main-js', get_template_directory_uri() . '/assets/main.js', [], '1.0.0', true );
+    wp_register_style( 'bootstrap-js', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js', ['jquery'], false, true  );
+
+    //Enqueue Styles
     wp_enqueue_style( 'style-css' );
+    wp_enqueue_style( 'bootstrap-css' );
+
+    //Enqueue Scripts
     wp_enqueue_script( 'main-js' );
+    wp_enqueue_script( 'bootstrap-js' );
 }
 add_action( 'wp_enqueue_scripts', 'themecurso_enqueue_scripts' );
