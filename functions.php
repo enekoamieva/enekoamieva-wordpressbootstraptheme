add65f0cf5fa3734311ec479f89b91d795974fd0
enekoamieva <enekoamieva@gmail.com>
2020-12-31 11:01:59 +0100

    08- Add custom menu with Bootstrap styles

--- functions.php
+++ functions.php
@@ -84,3 +84,46 @@ function bootstraptheme_setup_theme() {
 
 }
 add_action( 'after_setup_theme', 'bootstraptheme_setup_theme' );
+
+
+/**
+ * Register Menu
+ */
+function bootstraptheme_register_menus() {
+    register_nav_menus([
+        'header-menu'   => __( 'Header Menu', 'bootstraptheme'),
+        'footer-menu'   => __( 'Footer Menu', 'bootstraptheme')
+    ]);
+}
+add_action( 'init', 'bootstraptheme_register_menus' );
+
+
+/**
+ * Get Menu ID
+ */
+function get_menu_id( $location ) {
+    //Get all locations
+    $locations = get_nav_menu_locations();
+
+    //Get object id by location
+    $menu_id = $locations[$location];
+
+    return !empty( $menu_id ) ? $menu_id : '' ;
+}
+
+/**
+ * Get Child Menu items
+ */
+function get_child_menu_items( $menu_array, $parent_id ) {
+    $child_menus = [];
+
+    if( ! empty( $menu_array ) && is_array( $menu_array ) ) {
+        foreach( $menu_array as $menu ) {
+            if( intval( $menu->menu_item_parent ) === $parent_id ) {
+                array_push( $child_menus, $menu );
+            }
+        }
+    }
+
+    return $child_menus;
+}
--- template-parts/header/nav.php
+++ template-parts/header/nav.php
@@ -8,6 +8,12 @@
 
 ?>
 
+<?php
+    //Obtain number of menu items specif menu
+    $header_menu_id = get_menu_id( 'header-menu' );
+    $header_menu = wp_get_nav_menu_items( $header_menu_id );
+?>
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
     
     <?php
@@ -22,28 +28,53 @@
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="#">Link</a>
-            </li>
-            <li class="nav-item dropdown">
-                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown
-                </a>
-                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="#">Action</a>
-                <a class="dropdown-item" href="#">Another action</a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item" href="#">Something else here</a>
-                </div>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-            </li>
-        </ul>
+        <?php if( !empty( $header_menu ) && is_array( $header_menu ) ): ?>
+
+            <ul class="navbar-nav mr-auto">
+                <?php
+                    foreach( $header_menu as $menu_item ) {
+                        //Check only parent items
+                        if( ! $menu_item->menu_item_parent ){
+                            //Save children items to the parent item ID
+                            $child_menu_items = get_child_menu_items( $header_menu, $menu_item->ID );
+                            $has_children = ! empty( $child_menu_items ) && is_array( $child_menu_items );
+
+                            //Print only de parent items menu
+                            if( ! $has_children ): ?>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="<?php echo esc_url( $menu_item->url ); ?>">
+                                        <?php echo esc_html( $menu_item->title ); ?>
+                                    </a>
+                                </li>
+
+                            <?php else: //Print the parent menu with children items ?>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="<?php echo esc_url( $menu_item->url ); ?>" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <?php echo esc_html( $menu_item->title ); ?>
+                                    </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <?php
+                                            foreach( $child_menu_items as $child_menu_item ) {
+                                                ?>
+                                                    <a class="dropdown-item" href="<?php echo esc_url( $child_menu_item->url ); ?>" class="drodown-item">
+                                                        <?php echo esc_html( $child_menu_item->title ); ?>
+                                                    </a>
+                                                <?php
+                                            }
+                                        ?>
+                                    </div>
+                                </li>
+                            <?php endif;
+
+                        }
+
+                    }
+                ?>
+                
+            </ul>
+
+        <?php endif; ?>
+        
 
         <form class="form-inline my-2 my-lg-0">
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
